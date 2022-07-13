c0fbd3158af0879ae36948cb523550580146e190
Matthew Davis <matc.davis@gmail.com>
2022-07-13 18:17:49 +0000

    edited for template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = (actual, expected) => {
   if (actual === expected) {
-    console.log("🖖🖖🖖 Assertion Passed: " + actual + " === " + expected);
+    console.log(`🖖🖖🖖 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🤢🤢🤢Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🤢🤢🤢 Assertion Failed: ${actual} !== ${expected}`);
   }
 //Using the ternary op is better?
 //  return ((actual === expected)  ? console.log("Assertion Passed: " + actual + " === " + expected) : console.log("Assertion Failed: " + actual + " !== " + expected))

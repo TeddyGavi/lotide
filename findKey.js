8d089fafc7833ff22f25eb8ee9e39442e4de55ec
Matthew Davis <matc.davis@gmail.com>
2022-07-20 17:44:32 +0000

    added a function that will take an object and a callback and return the key that satisfies the callback function

---
+++ findKey.js
@@ -0,0 +1,43 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`🖖🖖🖖 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤢🤢🤢 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const starObject = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }}
+
+
+  //this is the solution to the original problem
+  const findKey = function(object, callback) {
+    for (const star in object) {
+    if (callback(object[star])) {
+      return star;
+    }
+  }
+}
+
+
+//this function returns a new objects populated with only the star objects that pass the test, it is essentailly filtering
+const findKeyAndReturnsAnObj = function(object, callback) {
+    const starList = {};
+    for (const star in object) {
+    if (callback(object[star])) {
+      starList[star] = object[star];
+    }
+  }
+  return starList;
+}
+
+console.log(findKey(starObject, x => x.stars === 2)) // => "noma"
+console.log(findKeyAndReturnsAnObj(starObject, x => x.stars === 2))
+assertEqual(findKey(starObject, x => x.stars === 2),  "noma"); //true
+assertEqual(findKey(starObject, x => x.stars === 1),  "Blue Hill"); //true
+assertEqual(findKey(starObject, x => x.stars === 3),  "Akaleri"); //true
+assertEqual(findKey(starObject, x => x.stars === 3),  "noma"); //false

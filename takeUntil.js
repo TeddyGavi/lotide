6977fbdf618803e1fd6c81d90fa183a0c44fdbcb
Matthew Davis <matc.davis@gmail.com>
2022-07-20 06:08:36 +0000

    added a function that takes an array and returns the values up until a certain specified rule is met

---
+++ takeUntil.js
@@ -0,0 +1,46 @@
+const eqArrays = (arrayOne, arrayTwo) => {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = (arrOne, arrTwo) => {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`Arrays are Equal, Hooray!`)
+  } else {
+    console.log(`Arrays are not Equal, Boooo`)
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (const item of array) {
+    if (!callback(item)) {
+    result.push(item);
+  } else {
+ return result;
+  }
+}
+}
+
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2])

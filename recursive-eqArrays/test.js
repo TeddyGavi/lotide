7ded078cd8c8fe2d083b1b1d26023035cfd609c5
Matthew Davis <matc.davis@gmail.com>
2022-07-29 05:32:54 +0000

    added tests that will surely fail to begin TDD

--- recursive-eqArrays/test.js
+++ recursive-eqArrays/test.js
@@ -3,3 +3,18 @@ const eqArraysRecursion = require('.');
 const expect = chai.expect;
 
 
+describe("#eqArraysRecursion, will compare two arrays and return true or false, regardless of the level of nested arrays present", () => {
+
+  it("should return true, given the same nested arrays", () => {
+    expect(eqArraysRecursion([[2, 3], [4]], [[2, 3], [4]])).to.be.true;
+  })
+
+  it("should return false given two arrays of unequal nesting", () => {
+    expect(eqArraysRecursion([[2, 3], [4]], [[2, 3], [4, 5]])).to.be.true;
+  })
+
+  it("should return false given two arrays of uequal nesting", () => {
+    expect(eqArraysRecursion([[2, 3], [4]], [[2, 3], 4])).to.be.true;
+  })
+
+})

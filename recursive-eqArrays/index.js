ccdc143fbf1cc1165fe4190692f7f1063c419924
Matthew Davis <matc.davis@gmail.com>
2022-07-30 04:07:47 +0000

    added more tests of recursion, added the addtional check for an undefined input into the function

--- recursive-eqArrays/index.js
+++ recursive-eqArrays/index.js
@@ -2,33 +2,34 @@ const isArray = (arr) => {
   if (Array.isArray(arr)) {
     return true;
   }
-}
+};
 
 
 const eqArraysRecursion = (arrayOne, arrayTwo) => {
-  if (arrayOne.length !== arrayTwo.length) {
+  if (!arrayOne || !arrayTwo) {
+    return false;
+  } else if (arrayOne.length !== arrayTwo.length) {
     return false;
   }
-    for (let i = 0; i < arrayOne.length; i++) {
-      if (isArray(arrayOne[i]) && isArray(arrayTwo[i])){
-        if (!eqArraysRecursion(arrayOne[i], arrayTwo[i])) {
-          return false;
-        }
-       } 
-      else if (arrayOne[i] !== arrayTwo[i]) {
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (isArray(arrayOne[i]) && isArray(arrayTwo[i])) {
+      if (!eqArraysRecursion(arrayOne[i], arrayTwo[i])) {
         return false;
-        }
-
+      }
+    } else if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
     }
-      return true;
+
+  }
+  return true;
 };
 
 
 
-console.log(eqArraysRecursion([2, 3], [2, 3]))
-console.log(eqArraysRecursion([[2, 3], [4]], [[2, 3], [4]])) // => true
-console.log(eqArraysRecursion([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
-console.log(eqArraysRecursion([[2, 3], [4]], [[2, 3], 4])) // => false
+// console.log(eqArraysRecursion([2, 3], [2, 3]))
+// console.log(eqArraysRecursion([[2, 3], [4]], [[2, 3], [4]])) // => true
+// console.log(eqArraysRecursion([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
+// console.log(eqArraysRecursion([[2, 3], [4]], [[2, 3], 4])) // => false
 
 
-module.exports = eqArraysRecursion
+module.exports = eqArraysRecursion;
--- recursive-eqArrays/test.js
+++ recursive-eqArrays/test.js
@@ -5,6 +5,10 @@ const expect = chai.expect;
 
 describe("#eqArraysRecursion, will compare two arrays and return true or false, regardless of the level of nested arrays present", () => {
 
+  it("should return true given two non nested arrays", () => {
+    expect(eqArraysRecursion([2, 3], [2, 3])).to.be.true;
+  })
+
   it("should return true, given the same nested arrays", () => {
     expect(eqArraysRecursion([[2, 3], [4]], [[2, 3], [4]])).to.be.true;
   })
@@ -15,6 +19,14 @@ describe("#eqArraysRecursion, will compare two arrays and return true or false, 
 
   it("should return false given two arrays of unequal nesting", () => {
     expect(eqArraysRecursion([[2, 3], [4]], [[2, 3], 4])).to.be.false;
+  })
+
+  it("should return true given two deeply nested equal arrays", () => {
+    expect(eqArraysRecursion([[2, 3 [[[4, 1]]], [[[[[5]]]]]]], [[2, 3 [[[4, 1]]], [[[[[5]]]]]]])).to.be.true;
+  })
+
+  it("should return false when given a undefiend input", () => {
+    expect(eqArraysRecursion([2, 4], undefined)).to.be.false;
   })
 
 })

73df0d3a8dd6bf2de6a7505020ebce82ed8b283c
Matthew Davis <matc.davis@gmail.com>
2022-08-03 05:28:50 +0000

    start ignoring assertEqual

--- assertEqual.js
+++
@@ -1,9 +0,0 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`🖖🖖🖖 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤢🤢🤢 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-module.exports = assertEqual;

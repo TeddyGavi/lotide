bfdd3859ac497155418be8c9e35a21e004ea0d5e
Matthew Davis <matc.davis@gmail.com>
2022-08-03 05:56:37 +0000

    start ignoring assertArraysEqualTest file

--- test/assertArraysEqualTest.js
+++
@@ -1,5 +0,0 @@
-const assertArraysEqual = require("../assertArraysEqual");
-
-// assertArraysEqual(["Hi", "Hi"], ["Hi", "Hi"]); // logging the hooray statement
-// assertArraysEqual(["H1i", "Hi"], ["Hi", "Hi"]); // logging the boooo statement
-// assertArraysEqual([1, 2], [1, 2]); // loging the hooray statement
